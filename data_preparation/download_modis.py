27f9022e4acd447a7886ca21bdf5c5f5e1294866
Fernand-Mouassom <fernand.mouassom@aims-cameroon.org>
2025-09-02 18:26:05 -0230

    updating

--- data_preparation/download_modis.py
+++ data_preparation/download_modis.py
@@ -15,7 +15,7 @@ auth = earthaccess.login()
 product_id = "MOD06_L2"
 start_date = "2000-01-01" # Change according to your study periode
 end_date = "2024-12-31"  # Change according to your study periode
-bounding_box = (-1, -7, 3, -3)  # Change according to your location
+bounding_box = (-1, -7, 3, -3)  # (min_lon, min_lat, max_lon, max_lat) Change according to your location
 product_id = ["MOD35_L2", "MOD06_L2"]
 output_folder = ["../MOD35_L2_data", "../MOD06_L2_data"] # Change as for your convenience 
 for k in range(len(product_id)):
